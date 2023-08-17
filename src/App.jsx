54f4ec8d662ccaaf31ac147ec0d1e1c55c45cad2
uthmancoder <akandeadebayo025@gmail.com>
2023-08-17 16:42:15 +0100

    done with the nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-
+import { BiMenuAltLeft } from "react-icons/bi";
+import { TfiAngleDown, TfiAngleUp } from "react-icons/tfi";
+import { useState } from "react";
 function App() {
+  const [categories, setcategories] = useState(false);
 
+  const handleToggle = () => {
+    setcategories(!categories);
+  };
   return (
     <>
-     <h1>Here is my homepage</h1>
-     <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, aut! Temporibus dolore iure quae, debitis perferendis impedit aut aliquam sint nihil omnis consequuntur? Eligendi, laborum. Cumque ea labore hic laudantium?</p>
+      <div className="w-full py-2 px-8 bg-yellow-500 h-16 relative flex items-center justify-between ">
+        <div className="bg-white rounded-t-md shadow-md   w-[320px]">
+          <div className="flex items-center border-b border-gray-200  justify-between w-full h-fit p-3">
+            <h1 className="text-md flex items-center font-semibold ">
+              <BiMenuAltLeft size={20} />
+              <span className="mx-3">All Categories</span>
+            </h1>
+            <div onClick={handleToggle}>
+              {!categories ? (
+                <TfiAngleDown size={15} />
+              ) : (
+                <TfiAngleUp size={15} />
+              )}
+            </div>
+          </div>
+          {categories ? (
+            <div className="m-3 duration-[2s] ease-in-out">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+              culpa explicabo magni facere nulla, veritatis nam doloremque unde
+              inventore distinctio ratione minus minima ullam, nisi laborum
+              architecto labore perspiciatis animi.
+            </div>
+          ) : null}
+        </div>
+        <div>
+          <ul className="flex items-center text-black-300">
+            <li className="p-4 font-semibold">Home</li>
+            <li className="p-4 font-semibold">About</li>
+            <li className="p-4 font-semibold">Contact</li>
+            <li className="p-4 font-semibold">Blog</li>
+          </ul>
+        </div>
+
+        <div>
+           <button className="bg-gray-800 text-gray-200 py-2 px-5 rounded-md hover:bg-slate-900 hover:text-gray-300 ">Become a seller</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,236 @@
-import React from "react";
+import React, { useState } from "react";
+import { TbCapture } from "react-icons/tb";
+import { RiRefreshLine } from "react-icons/ri";
+import {
+  AiOutlineHeart,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiOutlineSearch,
+} from "react-icons/ai";
+import { BiMobileVibration } from "react-icons/bi";
+import  {LiaAngleRightSolid} from "react-icons/lia"
 
 const NavBar = () => {
+  const [menu, setmenu] = useState(false);
+
+  const handleMenu = () => {
+    setmenu(!menu);
+  };
   return (
     <div>
-      <div>
-        <ul>
-          <li className="font-semibold text-xs">Account</li>
-          <li className="font-semibold text-xs">Track Order</li>
-          <li className="font-semibold text-xs">Support</li>
+      <div className="w-full flex justify-between md:px-6 items-center bg-gray-100 mx-auto  md:flex lg:flex">
+        <ul className="flex items-center ">
+          <li className="font-semibold text-xs p-4">Account</li>
+          <li className="font-semibold text-xs p-4">Track Order</li>
+          <li className="font-semibold text-xs p-4">Support</li>
         </ul>
-        <div>
-          <select name="location" id=""></select>
+        <div className="flex items-center hidden   md:flex lg:flex">
+          <select
+            className=" border border-gray-300 rounded-sm m-4"
+            name="location"
+            id="location"
+          >
+            <option value="Nigeria">Nigeria</option>
+            <option value="United State">United State</option>
+            <option value="Canada">Canada</option>
+          </select>
+          <select
+            className=" border border-gray-300 rounded-sm m-4"
+            name="currency"
+            id="currency"
+          >
+            <option value="NAIRA">NAIRA</option>
+            <option value="USD">USD</option>
+            <option value="EURO">EURO</option>
+          </select>
+          <select
+            className=" border border-gray-300 rounded-sm m-4"
+            name="language"
+            id="language"
+          >
+            <option value="English">English</option>
+            <option value="Arabic">Arabic</option>
+            <option value="French">French</option>
+          </select>
         </div>
       </div>
+
+      {/* Nav2 */}
+      <div className="w-full flex justify-between items-center px-6 bg-gray-50 py-3 mx-auto">
+        {/* logo-section */}
+        <div className=" flex items-center">
+          <TbCapture size={40} />
+          <h1 className="text-yellow-500 text-4xl font-extrabold font-poppins">
+            Shopp<span className="text-4xl text-black font-extrabold">ify</span>{" "}
+          </h1>
+        </div>
+
+        <div className="border border-gray-300 rounded-sm  w-fit h-fit hidden lg:block ">
+          <input
+            type="search"
+            className="p-2 w-[350px] bg-none outline-none cursor-pointer"
+            name="search"
+            id="search"
+            placeholder="Search Products"
+          />
+          <button className="bg-yellow-500 py-2 px-5 cursor-pointer  text-dark hover:bg-yellow-600 hover:text-gray-200 font-semibold">
+            Search
+          </button>
+        </div>
+
+        <div className="flex items-center">
+          <ul className="lg:flex  sm:flex  items-center hidden md:flex ">
+            <li className="p-4">
+              <RiRefreshLine size={20} />
+            </li>
+            <li className="p-4">
+              <AiOutlineHeart size={20} />
+            </li>
+            <li className="p-4 hidden sm:block">
+              <AiOutlineShoppingCart size={20} />
+            </li>
+            <li className="p-4">
+              {" "}
+              <AiOutlineUser size={20} />
+            </li>
+          </ul>
+          <div onClick={handleMenu} className="p-4 block md:hidden">
+            {!menu ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />}
+          </div>
+        </div>
+      </div>
+
+      {/* medium screen Sidenav */}
+
+      {menu ? (
+        <div className="w-[60%] bg-slate-200 fixed duration-[0.7s] ease-in-out top-0 left-0 h-screen ">
+          <ul className="flex justify-end ">
+            <li className="p-4">
+              <RiRefreshLine size={20} />
+            </li>
+            <li className="p-4">
+              <AiOutlineHeart size={20} />
+            </li>
+          </ul>
+          <div className=" flex items-center m-4">
+            <TbCapture size={40} />
+            <h1 className="text-yellow-500 text-4xl font-extrabold font-poppins">
+              Shopp
+              <span className="text-4xl text-black font-extrabold">
+                ify
+              </span>{" "}
+            </h1>
+          </div>
+          {/* Search */}
+          <div className="border flex items-center border-gray-300 rounded-sm  w-fit h-fit mx-auto ">
+            <input
+              type="search"
+              className="p-2  bg-none outline-none cursor-pointer"
+              name="search"
+              id="search"
+              placeholder="Search Products"
+            />
+            <button className="bg-yellow-500 py-[11px] px-2  mx-auto cursor-pointer  text-dark hover:bg-yellow-600 hover:text-gray-200 font-semibold">
+              <AiOutlineSearch />
+            </button>
+          </div>
+
+          {/* lists */}
+          <h1 className="m-3 text-gray-800 font-semibold text-2xl">
+            Categories
+          </h1>
+
+          {/* categories items */}
+          <div className="flex items-center justify-between p-5  ">
+            <BiMobileVibration/>
+            <h1>Mobile & Laptops</h1>
+            <LiaAngleRightSolid/>
+          </div>
+          <div className="flex items-center justify-between p-5  ">
+            <BiMobileVibration/>
+            <h1>Mobile & Laptops</h1>
+            <LiaAngleRightSolid/>
+          </div>
+          <div className="flex items-center justify-between p-5  ">
+            <BiMobileVibration/>
+            <h1>Mobile & Laptops</h1>
+            <LiaAngleRightSolid/>
+          </div>
+          <div className="flex items-center justify-between p-5  ">
+            <BiMobileVibration/>
+            <h1>Mobile & Laptops</h1>
+            <LiaAngleRightSolid/>
+          </div>
+          <div className="flex items-center justify-between p-5  ">
+            <BiMobileVibration/>
+            <h1>Mobile & Laptops</h1>
+            <LiaAngleRightSolid/>
+          </div>
+          <div className="flex items-center justify-between p-5  ">
+            <BiMobileVibration/>
+            <h1>Mobile & Laptops</h1>
+            <LiaAngleRightSolid/>
+          </div>
+          <div className="flex items-center justify-between p-5  ">
+            <BiMobileVibration/>
+            <h1>Mobile & Laptops</h1>
+            <LiaAngleRightSolid/>
+          </div>
+          <div className="flex items-center justify-between p-5  ">
+            <BiMobileVibration/>
+            <h1>Mobile & Laptops</h1>
+            <LiaAngleRightSolid/>
+          </div>
+        </div>
+      ) : (
+        <div className="w-[60%] fixed left-[-60%] ">
+          <div className=" flex items-center m-4">
+            <TbCapture size={40} />
+            <h1 className="text-yellow-500 text-4xl font-extrabold font-poppins">
+              Shopp
+              <span className="text-4xl text-black font-extrabold">
+                ify
+              </span>{" "}
+            </h1>
+          </div>
+          <ul className="flex items-center ">
+            <li className="font-semibold text-xs p-4">Account</li>
+            <li className="font-semibold text-xs p-4">Track Order</li>
+            <li className="font-semibold text-xs p-4">Support</li>
+          </ul>
+          <div className="grid items-center ">
+            <select
+              className=" border border-gray-300 rounded-sm m-4"
+              name="location"
+              id="location"
+            >
+              <option value="Nigeria">Nigeria</option>
+              <option value="United State">United State</option>
+              <option value="Canada">Canada</option>
+            </select>
+            <select
+              className=" border border-gray-300 rounded-sm m-4"
+              name="currency"
+              id="currency"
+            >
+              <option value="NAIRA">NAIRA</option>
+              <option value="USD">USD</option>
+              <option value="EURO">EURO</option>
+            </select>
+            <select
+              className=" border border-gray-300 rounded-sm m-4"
+              name="language"
+              id="language"
+            >
+              <option value="English">English</option>
+              <option value="Arabic">Arabic</option>
+              <option value="French">French</option>
+            </select>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
